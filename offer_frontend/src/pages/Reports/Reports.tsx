989c57ea205f148bf19584b16ac9852784a60f84
Ranil Sadykov <87027444+Shy11er@users.noreply.github.com>
2025-03-29 19:56:42 +0500

    #7 (#9)
    * #7
    
    * #7
    
    * #7
    
    * #7

--- offer_frontend/src/pages/Reports/Reports.tsx
+++ offer_frontend/src/pages/Reports/Reports.tsx
@@ -26,65 +26,70 @@ export const Reports: React.FC = () => {
 
     return (
         <div className={b()}>
-            <h1 className={b('heading')}>Подписанные договора</h1>
-            <table className={b('table')}>
-                <thead>
-                    <tr>
-                        <th className={b('th')}>Данные</th>
-                        <th className={b('th')}>Адрес</th>
-                        <th className={b('th')}>Статус</th>
-                        <th className={b('th')} />
-                    </tr>
-                </thead>
-                <tbody>
-                    {contracts.map((contract, key) => (
-                        <>
-                            <tr key={key} className={b('row')}>
-                                <td
-                                    style={{
-                                        display: 'flex',
-                                        flexDirection: 'column',
-                                        fontSize: '13px',
-                                    }}
-                                    className={b('cell')}
+            <h1 className={b('heading')}>
+                {contracts?.length === 0 ? 'У вас пока нет договоров' : 'Подписанные договора'}
+            </h1>
+            {contracts?.length !== 0 && (
+                <table className={b('table')}>
+                    <thead>
+                        <tr>
+                            <th className={b('th')}>Данные</th>
+                            <th className={b('th')}>Адрес</th>
+                            <th className={b('th')}>Статус</th>
+                            <th className={b('th')} />
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {contracts.map((contract, key) => (
+                            <>
+                                <tr key={key} className={b('row')}>
+                                    <td
+                                        style={{
+                                            display: 'flex',
+                                            flexDirection: 'column',
+                                            fontSize: '13px',
+                                        }}
+                                        className={b('cell')}
+                                    >
+                                        <Text variant="body-1">{contract.fullName}</Text>
+                                        <Text variant="caption-1">
+                                            {contract.passportSeries}, {contract.passportNumber}
+                                        </Text>
+                                    </td>
+                                    <td className={b('cell')}>
+                                        <Text variant="body-1">{contract.address || '-'}</Text>
+                                    </td>
+                                    <td className={b('cell')}>
+                                        <div className={b('status')}>
+                                            {contract.signedAt ? (
+                                                <Text variant="caption-1" color="positive">
+                                                    Подписано <br />
+                                                    {dayjs(contract.signedAt).format(
+                                                        'DD.MM.YYYY',
+                                                    )}{' '}
+                                                    в {dayjs(contract.signedAt).format('HH:mm')}
+                                                </Text>
+                                            ) : (
+                                                <Text variant="caption-1" color="warning">
+                                                    Не подписано <br />
+                                                </Text>
+                                            )}
+                                        </div>
+                                    </td>
+                                </tr>
+                                <Button
+                                    view="action"
+                                    size="xl"
+                                    style={{color: 'white'}}
+                                    onClick={() => navigate(`/contracts/${contract.objectDto?.id}`)}
                                 >
-                                    <Text variant="body-1">{contract.fullName}</Text>
-                                    <Text variant="caption-1">
-                                        {contract.passportSeries}, {contract.passportNumber}
-                                    </Text>
-                                </td>
-                                <td className={b('cell')}>
-                                    <Text variant="body-1">{contract.address || '-'}</Text>
-                                </td>
-                                <td className={b('cell')}>
-                                    <div className={b('status')}>
-                                        {contract.signedAt ? (
-                                            <Text variant="caption-1" color="positive">
-                                                Подписано <br />
-                                                {dayjs(contract.signedAt).format(
-                                                    'DD.MM.YYYY',
-                                                )} в {dayjs(contract.signedAt).format('HH:mm')}
-                                            </Text>
-                                        ) : (
-                                            <Text variant="caption-1" color="warning">
-                                                Не подписано <br />
-                                            </Text>
-                                        )}
-                                    </div>
-                                </td>
-                            </tr>
-                            <Button
-                                view="action"
-                                size="xl"
-                                style={{color: 'white'}}
-                                onClick={() => navigate(`/contracts/${contract.objectDto?.id}`)}
-                            >
-                                Посмотреть договор
-                            </Button>
-                        </>
-                    ))}
-                </tbody>
-            </table>
+                                    Посмотреть договор
+                                </Button>
+                            </>
+                        ))}
+                    </tbody>
+                </table>
+            )}
         </div>
     );
 };
